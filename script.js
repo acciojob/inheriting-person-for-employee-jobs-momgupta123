11da505925f39409d6c4929cb900e14732698928
Raushan kumar <98281059+momgupta123@users.noreply.github.com>
2023-11-23 01:56:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
-// Person constructor function
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-// Adding greet method to the Person prototype
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-};
-
-// Employee constructor function that inherits from Person
-function Employee(name, age, jobTitle) {
-  // Call the parent constructor using 'call' to set the context
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
-}
-
-// Inherit the Person prototype in the Employee prototype
-Employee.prototype = Object.create(Person.prototype);
-
-// Adding jobGreet method to the Employee prototype
-Employee.prototype.jobGreet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-};
-
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+// Cypress test code
+cy.stub(win.console, "log").as("consoleLog");
+person.greet();
+cy.get("@consoleLog").should("be.calledWith", `Hello, my name is Alice, I am 25 years old.`); // Updated assertion
+employee.jobGreet();
+cy.get("@consoleLog").should("be.calledWith", `Hello, my name is Bob, I am 30 years old, and my job title is Manager.`); // Updated assertion
